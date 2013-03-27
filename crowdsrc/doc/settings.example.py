af266ef8fd4c9f502fa3dd9bbb14f5fb8685efc2
Mircea Grecu <cypher.com@gmail.com>
2013-03-27 17:52:05 +0100

    Updated settings.example.py

--- crowdsrc/doc/settings.example.py
+++ crowdsrc/doc/settings.example.py
@@ -80,9 +80,7 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    STATIC_PATH,
 )
 
 # List of finder classes that know how to find static files in
