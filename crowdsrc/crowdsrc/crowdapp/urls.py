d5dbaaf9f4bdb59f73fec1c5ee8b24047fe97630
Mircea Grecu <cypher.com@gmail.com>
2013-04-06 13:35:24 +0200

    Removing deprecated function call "redirect_to".

--- crowdsrc/crowdsrc/crowdapp/urls.py
+++ crowdsrc/crowdsrc/crowdapp/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-from django.views.generic.simple import redirect_to
+from django.views.generic import RedirectView
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -12,7 +12,7 @@ urlpatterns = patterns('',
     url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'auth/login.html'}),
     url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
     url(r'^register/$', 'crowdapp.views.register'),
-    url(r'^accounts/profile/$', redirect_to, {'url': '/'}),
+    url(r'^accounts/profile/$', RedirectView.as_view(url='/', permanent=True)),
 
     # Task related urls
     url(r'^create_task/$', 'crowdapp.views.create_task'),
