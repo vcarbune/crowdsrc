93a290876ab30d2e5515dac5f91fd6f4598fc099
Mircea Grecu <cypher.com@gmail.com>
2013-04-14 21:32:35 +0200

    Toolbox items CRUD + Simple templates + Sortable toolbox item list

--- crowdsrc/static/js/toolbox-items.js
+++ crowdsrc/static/js/toolbox-items.js
@@ -57,3 +57,67 @@ function TaskInputCtrl($scope, toggleToolboxStateService)
     toggleToolboxStateService: toggleToolboxStateService  
   });
 };
+
+/**
+ * Toolbox - Text Field Controller.
+ */
+function ParagraphCtrl($scope, toggleToolboxStateService)
+{
+  angular.injector().invoke(ToolboxItemCtrl, this, {
+    $scope: $scope,
+    toggleToolboxStateService: toggleToolboxStateService  
+  });
+};
+
+/**
+ * Toolbox - Text Field Controller.
+ */
+function TextFieldCtrl($scope, toggleToolboxStateService)
+{
+  angular.injector().invoke(ToolboxItemCtrl, this, {
+    $scope: $scope,
+    toggleToolboxStateService: toggleToolboxStateService  
+  });
+};
+
+/**
+ * Toolbox - Checkbox Controller.
+ */
+function CheckboxCtrl($scope, toggleToolboxStateService)
+{
+  angular.injector().invoke(ToolboxItemCtrl, this, {
+    $scope: $scope,
+    toggleToolboxStateService: toggleToolboxStateService  
+  });
+};
+
+/**
+ * Toolbox - Ranking Comp Controller.
+ */
+function RadioGroupCtrl($scope, toggleToolboxStateService)
+{
+  angular.injector().invoke(ToolboxItemCtrl, this, {
+    $scope: $scope,
+    toggleToolboxStateService: toggleToolboxStateService  
+  });
+  
+  $scope.items = []
+  $scope.selectedItem = null;
+  
+  $scope.addItem = function() {
+	$scope.items.push({
+		id: $scope.items.length,
+		name: "New item"
+	});
+  };
+  
+  $scope.removeItem = function(id) {
+	  for (var i=0;i<$scope.items.length;i++) { 
+		  if ($scope.items[i].id == id) {
+			  $scope.items.splice(i, 1);
+			  break;
+		  }
+	  }
+  };
+  
+};
--- crowdsrc/static/js/toolbox.js
+++ crowdsrc/static/js/toolbox.js
@@ -1,5 +1,5 @@
 // Toolbox Controllers and HTML Structures
-var app = angular.module('angularjs-toolbox', []);
+var app = angular.module('angularjs-toolbox', ['ui']);
 
 // This service broadcasts to each component whether it needs to change
 // its state or not (e.g. preview for workers or edit for creators)
@@ -36,29 +36,67 @@ app.directive('toolboxItem', function($compile) {
   // requires different angular JS directives (needs change in ToolboxCtrl also).
 
   // Note: not really important right now.
+  
+  /*
   var descriptionItemTemplate =
       "<div ng-controller='TaskDescriptionCtrl' class='task-generic-item'>" +
-      "<b>Task:</b><div ng-model='desc' contenteditable='{{isEditable}}'></div>" +
+      "<b>Task:</b><div ng-model='desc' contenteditable='{{isEditable}}'>Add text here...</div>" +
       "</div>";
 
   var inputItemTemplate =
       "<div ng-controller='TaskInputCtrl' class='task-generic-item'>" +
       "<b>Solution:</b> <input id='task_{{content.crt}}' type='text' ng-disabled='disabled' />" +
       "</div>";
-
+  
   var submitItemTemplate =
       "<div ng-controller='TaskSubmitCtrl' class='task-generic-item'>" +
       "<button ng-click='completeTask()' ng-disabled='disabled'>Submit</button>" +
       "</div>";
+  */
 
-  var getTemplate = function(taskElementType) {
+  var paragraphTemplate = 
+	  "<div ng-controller='ParagraphCtrl' class='task-generic-item'>" +
+      "<p for='task_{{content.crt}}' contenteditable='{{isEditable}}' class='editable'>Add text here...</p>" + 
+      "</div>";
+  
+  var textFieldTemplate =
+	  "<div ng-controller='TextFieldCtrl' class='task-generic-item'>" +
+      "<label for='task_{{content.crt}}' contenteditable='{{isEditable}}' class='editable'>Label:</label>" + 
+      "<input id='task_{{content.crt}}' name='task_{{content.crt}}' type='text' ng-disabled='disabled' />" +
+      "</div>";
+  
+  var checkboxTemplate = 
+	  "<div ng-controller='CheckboxCtrl' class='task-generic-item'>" +
+      "<input id='task_{{content.crt}}' name='task_{{content.crt}}' type='checkbox' ng-disabled='disabled' />" +
+      "<label for='task_{{content.crt}}' contenteditable='{{isEditable}}' class='editable'>Label</label>" +
+      "</div>";
+  
+  var radioGroupTemplate = 
+	  "<div ng-controller='RadioGroupCtrl' class='task-generic-item'>" +
+	  "<div ng:repeat='i in items'>" +
+      "<input type='radio' value='{{i.id}}' name='task_{{content.crt}}' id='radio_{{i.id}}' ng-disabled='disabled' />" +
+      "<label for='radio_{{i.id}}' contenteditable='{{isEditable}}' class='editable'>{{i.name}}</label>" +
+      "<button type='button' ng-click='removeItem({{i.id}})' ng-show='isEditable'>Remove Item</button>" +
+      "</div>" +
+      "<button type='button' ng-click='addItem()' ng-show='isEditable'>Add Item</button>" +
+      "</div>";
+  
+  var getTemplate = function(taskElementType) { 
     switch(taskElementType) {
-      case 'descriptionItem':
-        return descriptionItemTemplate;
-      case 'inputItem':
-        return inputItemTemplate;
-      case 'submitItem':
-        return submitItemTemplate;
+//      case 'descriptionItem':
+//        return descriptionItemTemplate;
+//      case 'inputItem':
+//        return inputItemTemplate;
+//      case 'submitItem':
+//        return submitItemTemplate;
+      case 'textField':
+    	return textFieldTemplate;
+      case 'checkbox':
+      	return checkboxTemplate;
+      case 'paragraph':
+    	return paragraphTemplate; 
+      case 'radioGroup':
+    	return radioGroupTemplate;  
     }
   };
 
@@ -88,7 +126,9 @@ function shuffle(o){ //v1.0
  * Toolbox - Master Controller
  */
 app.controller('ToolboxCtrl', function($scope, toggleToolboxStateService) {
+	
   $scope.state = toggleToolboxStateService.STATE.EDIT;
+  
   $scope.toggleState = function() {
     if ($scope.state == toggleToolboxStateService.STATE.EDIT)
       $scope.state = toggleToolboxStateService.STATE.PREVIEW;
@@ -97,14 +137,25 @@ app.controller('ToolboxCtrl', function($scope, toggleToolboxStateService) {
 
     toggleToolboxStateService.setState($scope.state);
   };
+  
+  $scope.elemTypes = [
+    {code:'textField', name: 'Text Field'},
+    {code:'paragraph', name: 'Paragraph'},
+    {code:'checkbox', name: 'Checkbox'},
+    {code:'radioGroup', name: 'Radio Group'}
+  ];
+  
+  $scope.newElemType = '';
 
   /* List of elements currently in the toolbox for the current task */
-  $scope.content = [
-  {
-    type: 'submitItem',
-    desc: 'Submit'
-  }];
-
+  $scope.content = [];
+  
+  $scope.sortableOptions = {
+	cancel: ':input,button,.editable',
+	axis: 'y'
+  };
+  
+  /*
   $scope.insertDescriptionElement = function() {
     $scope.content.unshift({
       type: 'descriptionItem',
@@ -128,4 +179,23 @@ app.controller('ToolboxCtrl', function($scope, toggleToolboxStateService) {
   $scope.addAnswerInput = function() {
      $scope.insertAnswerElement();
   };
+  */
+  
+  $scope.addElement = function() {
+	  $scope.content.push({
+	    type: $scope.newElemType,
+	    desc: '',
+	    crt: $scope.content.length
+	  });
+  };
+  
+  $scope.removeElement = function (crt) {
+	  for (var i=0;i<$scope.content.length;i++) { 
+		  if ($scope.content[i].crt == crt) {
+			  $scope.content.splice(i, 1);
+			  break;
+		  }
+	  }
+  };
+  
 });
