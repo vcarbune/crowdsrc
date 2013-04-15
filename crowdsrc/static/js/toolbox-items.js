92ee5ef8a9c5544048ef3b5cfaadb65e1df2aeea
Mircea Grecu <cypher.com@gmail.com>
2013-04-15 14:46:51 +0200

    Simple ranking component + Some fixes

--- crowdsrc/static/js/toolbox-items.js
+++ crowdsrc/static/js/toolbox-items.js
@@ -26,39 +26,6 @@ function ToolboxItemCtrl($scope, toggleToolboxStateService)
 }
 
 /**
- * Toolbox - Description Item Controller.
- */
-function TaskDescriptionCtrl($scope, toggleToolboxStateService)
-{
-  angular.injector().invoke(ToolboxItemCtrl, this, {
-    $scope: $scope,
-    toggleToolboxStateService: toggleToolboxStateService  
-  });
-};
-
-/**
- * Toolbox - Submit Item Controller.
- */
-function TaskSubmitCtrl($scope, toggleToolboxStateService)
-{
-  angular.injector().invoke(ToolboxItemCtrl, this, {
-    $scope: $scope,
-    toggleToolboxStateService: toggleToolboxStateService
-  });
-};
-
-/**
- * Toolbox - Input Item Controller.
- */
-function TaskInputCtrl($scope, toggleToolboxStateService)
-{
-  angular.injector().invoke(ToolboxItemCtrl, this, {
-    $scope: $scope,
-    toggleToolboxStateService: toggleToolboxStateService  
-  });
-};
-
-/**
  * Toolbox - Text Field Controller.
  */
 function ParagraphCtrl($scope, toggleToolboxStateService)
@@ -92,7 +59,7 @@ function CheckboxCtrl($scope, toggleToolboxStateService)
 };
 
 /**
- * Toolbox - Ranking Comp Controller.
+ * Toolbox - Radio Group Controller.
  */
 function RadioGroupCtrl($scope, toggleToolboxStateService)
 {
@@ -112,12 +79,81 @@ function RadioGroupCtrl($scope, toggleToolboxStateService)
   };
   
   $scope.removeItem = function(id) {
-	  for (var i=0;i<$scope.items.length;i++) { 
+	  for (var i=0; i<$scope.items.length; i++) {
 		  if ($scope.items[i].id == id) {
+			  for (var j=0; j<$scope.items.length; j++) {
+				  if ($scope.items[j].id > id) {
+					  $scope.items[j].id--;
+				  }
+			  }
+			  $scope.items.splice(i, 1);
+			  break;
+		  }
+	  }
+  };
+};
+
+/**
+ * Toolbox - Ranking Component Controller.
+ */
+function RankingCtrl($scope, toggleToolboxStateService)
+{
+  angular.injector().invoke(ToolboxItemCtrl, this, {
+    $scope: $scope,
+    toggleToolboxStateService: toggleToolboxStateService  
+  });
+  
+  $scope.items = []
+  $scope.orderedItems = [];
+  $scope.currentRank = 0;
+  
+  $scope.addItem = function() {
+	$scope.items.push({
+		id: $scope.items.length,
+		name: 'New item',
+		rank: 100, // big enough
+		state: 'free',
+	});
+  };
+  
+  $scope.removeItem = function(id) {
+	  for (var i=0; i<$scope.items.length; i++) {
+		  if ($scope.items[i].id == id) {
+			  for (var j=0; j<$scope.items.length; j++) {
+				  if ($scope.items[j].id > id) {
+					  $scope.items[j].id--;
+				  }
+			  }
 			  $scope.items.splice(i, 1);
 			  break;
 		  }
 	  }
   };
   
+  $scope.toggleSelectItem = function(id) {
+	  if ($scope.isEditable === true) {
+		  return
+	  }
+	  
+	  for (var i=0; i<$scope.items.length; i++) { 
+		  if ($scope.items[i].id == id) {
+			  var item = $scope.items[i];
+			  if (item.state === 'selected') {
+				  item.state = 'free';
+				  for (var j=0; j<$scope.items.length; j++) {
+					  if ($scope.items[j].state === 'selected' && $scope.items[j].rank > item.rank) {
+						  $scope.items[j].rank--;
+					  }
+				  }
+				  item.rank = 0;
+				  $scope.currentRank--;
+			  }
+			  else {
+				  item.state = 'selected';
+				  item.rank = ++$scope.currentRank;
+			  }
+			  break;
+		  }
+	  }
+  }
 };
--- crowdsrc/static/js/toolbox.js
+++ crowdsrc/static/js/toolbox.js
@@ -58,9 +58,21 @@ app.directive('toolboxItem', function($compile) {
 	  "<div ng:repeat='i in items'>" +
       "<input type='radio' value='{{i.id}}' name='task_{{content.crt}}' id='radio_{{i.id}}' ng-disabled='disabled' />" +
       "<label for='radio_{{i.id}}' contenteditable='{{isEditable}}' class='editable'>{{i.name}}</label>" +
-      "<button type='button' ng-click='removeItem({{i.id}})' ng-show='isEditable'>Remove Item</button>" +
+      "<button type='button' ng-click='removeItem(i.id)' ng-show='isEditable'>Remove Item</button>" +
       "</div>" +
       "<button type='button' ng-click='addItem()' ng-show='isEditable'>Add Item</button>" +
+      "</div>";
+  
+  var rankingTemplate = 
+	  "<div ng-controller='RankingCtrl' class='task-generic-item'>" +
+	  "<ul class='toolbox-ranking-list'>" +
+	  "<li ng-repeat='i in items' class='{{i.state}}'>" +
+	  	"<span class='toolbox-ranking-name editable' contenteditable='{{isEditable}}' ng-click='toggleSelectItem(i.id)'>{{i.name}}</span>" +
+	  	"<span class='toolbox-ranking-rank' ng-hide='isEditable || i.state===\"free\"'> - {{i.rank}}</span>" +
+	  	"<button type='button' ng-click='removeItem(i.id)' ng-show='isEditable'>Remove Item</button>" +
+	  "</li>" +
+      "</ul>" +
+      "<br /><button type='button' ng-click='addItem()' ng-show='isEditable'>Add Item</button>" +
       "</div>";
   
   var getTemplate = function(taskElementType) { 
@@ -73,6 +85,8 @@ app.directive('toolboxItem', function($compile) {
     	  return paragraphTemplate; 
       case 'radioGroup':
     	  return radioGroupTemplate;  
+      case 'ranking':
+    	  return rankingTemplate; 
     }
   };
 
@@ -111,7 +125,8 @@ app.controller('ToolboxCtrl', function($scope, toggleToolboxStateService) {
     {code:'textField', name: 'Text Field'},
     {code:'paragraph', name: 'Paragraph'},
     {code:'checkbox', name: 'Checkbox'},
-    {code:'radioGroup', name: 'Radio Group'}
+    {code:'radioGroup', name: 'Radio Group'},
+    {code:'ranking', name: 'Ranking Component'}
   ];
   
   $scope.newElemType = '';
@@ -126,19 +141,23 @@ app.controller('ToolboxCtrl', function($scope, toggleToolboxStateService) {
   
   $scope.addElement = function() {
 	  $scope.content.push({
-	    type: $scope.newElemType,
-	    desc: '',
-	    crt: $scope.content.length
+		  id: $scope.content.length,
+		  type: $scope.newElemType,
+		  desc: '',
 	  });
   };
   
-  $scope.removeElement = function (crt) {
-	  for (var i=0;i<$scope.content.length;i++) { 
-		  if ($scope.content[i].crt == crt) {
+  $scope.removeElement = function (id) {
+	  for (var i=0; i<$scope.content.length; i++) {
+		  if ($scope.content[i].id == id) {
+			  for (var j=0; j<$scope.content.length; j++) {
+				  if ($scope.content[j].id > id) {
+					  $scope.content[j].id--;
+				  }
+			  }
 			  $scope.content.splice(i, 1);
 			  break;
 		  }
 	  }
   };
-  
 });
